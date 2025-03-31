f7585c286debd87e47bf36aae2aa4502cff12461
Zetsuki <zetsukiOG@gmail.com>
2025-03-31 14:28:21 +0200

    Added mock and some endpoints

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require('express')
+let pokemons = require('./mock-pokemon')
 
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello, Express !'))
+
+app.get('/api/pokemons/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const pokemon = pokemons.find(pokemon => pokemon.id === id)
+    res.send(`Le pokémon est ${pokemon.name}`)
+})
+
+app.get('/api/pokemons', (req, res) => {
+    res.send(`Il y a actuellement ${pokemons.length} pokémon dans le pokedex.`)
+})
 
 app.listen(port, () => console.log(`Node app started on : http://localhost:${port}`))

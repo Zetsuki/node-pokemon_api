d65970642bbb9e7308562d9c624febe58dfced1d
Zetsuki <zetsukiOG@gmail.com>
2025-03-31 14:05:41 +0200

    Added express using 'npm install express --save' and modified app.js accordingly

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log('Hello, node !')
+const express = require('express')
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello, Express !'))
+
+app.listen(port, () => console.log(`Node app started on : http://localhost:${port}`))

6f2d0ab6c04463bbf3a131bd1d4ece432f576bc1
Zetsuki <zetsukiOG@gmail.com>
2025-04-10 11:57:36 +0200

    More advanced requests using sequelize operators

--- src/routes/findAllPokemons.js
+++ src/routes/findAllPokemons.js
@@ -1,8 +1,33 @@
 const { Pokemon } = require('../db/sequelize')
+const { Op } = require('sequelize')
   
 module.exports = (app) => {
   app.get('/api/pokemons', (req, res) => {
-    Pokemon.findAll()
+    if(req.query.name) {
+      const name = req.query.name
+      const limit = parseInt(req.query.limit) || 5
+
+      if(name.length < 2) {
+        const message = 'Le terme de recherche doit contenir au moins 2 caractères.'
+        res.status(400).json({ message })
+      }
+
+      return Pokemon.findAndCountAll({ 
+        where: { 
+          name: {
+            [Op.like]: `%${name}%`
+          }
+        },
+        order: ['name'], 
+        limit: limit
+      })
+      .then(({count, rows}) => {
+        const message = `Il y a ${count} pokémons qui correspondent au terme de recherche ${name}`
+        res.json({message, data: rows })
+      })
+    }
+    else {
+      Pokemon.findAll()
       .then(pokemons => {
         const message = 'La liste des pokémons a bien été récupérée.'
         res.json({ message, data: pokemons })
@@ -11,5 +36,6 @@ module.exports = (app) => {
         const message = `La liste des pokémons n'a pas pu être récupérée. Réssayez dans quelque instants.`
         res.status(500).json({ message, data: error }) 
       })
+    }
   })
 } 
